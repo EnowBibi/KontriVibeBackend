a1c6ae81f655561d35b93b585ac66f13254982fc
=Val242 <=ebongvalentine70@gmail.com>
2025-11-17 10:09:14 -0600

    Created Mongodb connection

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-// Server.js
+import dotenv from "dotenv";
+import connectToDB from './config/db.js'
+dotenv.config();
+
+
+const PORT = 3000;
+
+
+connectToDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+});
