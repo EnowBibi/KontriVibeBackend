ff2351449a85774dd7bdd269a7e39d25fe11f443
Enowtabi <enowbibi@gmail.com>
2025-11-18 00:05:12 +0100

    implemented email verification

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -64,7 +64,7 @@ export const registerUser = async (req, res) => {
     });
   }
 };
-
+// to verify email verification code
 export const verifyCode = async (req, res) => {
   try {
     const { userId, code } = req.body;
