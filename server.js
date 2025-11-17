2b34da972ddc25d217594831557871b58b3cd77b
Enowtabi <enowbibi@gmail.com>
2025-11-17 11:43:57 +0100

    successful merge 1

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import dotenv from "dotenv";
 import connectToDB from "./config/db.js";
+import aiRoutes from "./routes/aiRoutes.js";
+
 dotenv.config();
 
 const PORT = 3000;
 
 connectToDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+    console.log(` Server running on port ${PORT}`);
   });
 });
-import aiRoutes from "./routes/aiRoutes.js";
 
 app.use("/api/ai", aiRoutes);
